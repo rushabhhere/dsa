8d5511e3e534859fc896d938ecfd6a5fec38b3f4
Rushabh Javeri <javeri.rushabh45@gmail.com>
2024-02-25 05:59:16 +0530

    added solved state, reset button, made it responsive

--- www/src/components/Sudoku/SudokuInput.tsx
+++ www/src/components/Sudoku/SudokuInput.tsx
@@ -1,24 +1,37 @@
-// component to take sudoku input
 import clsx from 'clsx';
 import { type FC } from 'react';
 
 interface Props {
   sudoku: string[][];
   setSudoku: React.Dispatch<React.SetStateAction<string[][]>>;
+  inputCells: boolean[][];
+  setInputCells: React.Dispatch<React.SetStateAction<boolean[][]>>;
+  solved: boolean;
 }
 
-const SudokuInput: FC<Props> = ({ sudoku, setSudoku }) => {
+const SudokuInput: FC<Props> = ({
+  sudoku,
+  setSudoku,
+  inputCells,
+  setInputCells,
+  solved,
+}) => {
   return (
-    <div className="w-[450px]">
+    <div className="w-[270px] sm:w-[450px]">
       <div className="grid grid-cols-9 border-black border-2 border-r-[1px] border-b-[1px]">
         {sudoku.map((row, i) =>
           row.map((cell, j) => (
             <input
               key={`${i}-${j}`}
+              disabled={solved}
               type="text"
               className={clsx(
-                'text-center border-[1px] border-black outline-none h-[50px]',
-                { 'border-r-2': j % 3 === 2, 'border-b-2': i % 3 === 2 }
+                'text-center font-bold text-xl sm:text-4xl border-[1px] border-black outline-none h-[30px] sm:h-[50px]',
+                {
+                  'border-r-2': j % 3 === 2,
+                  'border-b-2': i % 3 === 2,
+                  'bg-gray-300': inputCells[i][j],
+                }
               )}
               value={cell === '.' ? '' : cell}
               onChange={e => {
@@ -35,6 +48,12 @@ const SudokuInput: FC<Props> = ({ sudoku, setSudoku }) => {
                   const newSudoku = [...prev];
                   newSudoku[i][j] = value.length === 0 ? '.' : value;
                   return newSudoku;
+                });
+
+                setInputCells((prev: boolean[][]) => {
+                  const newInputCells = [...prev];
+                  newInputCells[i][j] = value.length !== 0;
+                  return newInputCells;
                 });
               }}
             />
--- www/src/pages/problems/sudoku.tsx
+++ www/src/pages/problems/sudoku.tsx
@@ -20,8 +20,51 @@ const Sudoku: FC<Props> = () => {
     ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
   ]);
 
+  const [inputCells, setInputCells] = useState<boolean[][]>([
+    [false, false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false, false],
+  ]);
+
+  const [solved, setSolved] = useState<boolean>(false);
+
   const solve = () => {
     setSudoku(solveSudoku(sudoku));
+    setSolved(true);
+  };
+
+  const reset = () => {
+    setSudoku([
+      ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
+      ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
+      ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
+      ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
+      ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
+      ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
+      ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
+      ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
+      ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
+    ]);
+
+    setInputCells([
+      [false, false, false, false, false, false, false, false, false],
+      [false, false, false, false, false, false, false, false, false],
+      [false, false, false, false, false, false, false, false, false],
+      [false, false, false, false, false, false, false, false, false],
+      [false, false, false, false, false, false, false, false, false],
+      [false, false, false, false, false, false, false, false, false],
+      [false, false, false, false, false, false, false, false, false],
+      [false, false, false, false, false, false, false, false, false],
+      [false, false, false, false, false, false, false, false, false],
+    ]);
+
+    setSolved(false);
   };
 
   return (
@@ -29,12 +72,24 @@ const Sudoku: FC<Props> = () => {
       <h1 className="text-2xl font-bold text-red-500">Sudoku Solver</h1>
 
       <div className="mt-5">
-        <SudokuInput sudoku={sudoku} setSudoku={setSudoku} />
+        <SudokuInput
+          sudoku={sudoku}
+          setSudoku={setSudoku}
+          inputCells={inputCells}
+          setInputCells={setInputCells}
+          solved={solved}
+        />
         <button
           onClick={solve}
           className="px-4 py-2 mt-5 text-lg bg-green-300 rounded-md hover:bg-green-400"
         >
           Solve
+        </button>
+        <button
+          onClick={reset}
+          className="px-4 py-2 mt-5 ml-3 text-lg bg-gray-200 rounded-md hover:bg-gray-300"
+        >
+          Reset
         </button>
       </div>
     </ProblemWrapper>
