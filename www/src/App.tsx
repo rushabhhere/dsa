406741b7c4efc07cf9211ba795b25184cca64512
Rushabh Javeri <javeri.rushabh45@gmail.com>
2024-02-25 06:27:50 +0530

    added 404 page

--- www/src/App.tsx
+++ www/src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Switch } from 'wouter';
+import { Link, Route, Switch } from 'wouter';
 import Home from './pages/home';
 import Sudoku from './pages/problems/sudoku';
 
@@ -8,6 +8,19 @@ function App() {
       <Switch>
         <Route path="/" component={Home} />
         <Route path="/sudoku" component={Sudoku} />
+        {/* 404 */}
+        <Route>
+          <main className="max-w-4xl px-5 py-10 mx-auto">
+            <h1 className="text-4xl font-bold">404</h1>
+            <p className="mt-3 mb-5 text-xl">Page not found</p>
+            <Link
+              className="px-4 py-2 text-lg text-white bg-blue-500 rounded-md"
+              to="/"
+            >
+              Go Home
+            </Link>
+          </main>
+        </Route>
       </Switch>
     </>
   );
